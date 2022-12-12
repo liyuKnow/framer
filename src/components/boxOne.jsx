186442fe90f585b7c6f6c7e544253e510d49280d
liyuKnow <kidus2011@gmail.com>
2022-12-12 07:37:13 -0800

    spring and dumping

--- src/components/boxOne.jsx
+++ src/components/boxOne.jsx
@@ -3,16 +3,25 @@ import { motion } from "framer-motion";
 
 const BoxOne = () => {
   return (
-    <div className="container mx-auto w-full h-[400px] p-4 bg-blue-100 flex flex-col justify-start items-start">
+    <div className="container mt-6 mx-auto w-full h-[400px] p-4 bg-blue-100 flex flex-col justify-start items-start">
       <motion.div
-        className="w-[300px] h-[300px] p-4 bg-green-200"
+        className="w-[300px] h-[300px] mt-6 relative p-4 bg-indigo-300"
         initial={{
-          opacity: 0.2,
+          opacity: 0.4,
         }}
         animate={{
           x: 600,
-          rotate: 180,
-          opacity: 0.9,
+          // rotate: 90,
+          opacity: 0.8,
+          backgroundColor: "green",
+        }}
+        transition={{
+          type: "spring",
+          stiffness: 20,
+          damping: 1,
+
+          // type: "tween",
+          // duration: 3,
         }}
       ></motion.div>
     </div>
