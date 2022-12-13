27fb86707e9297fec01cab2869a058a308e5f27e
liyuKnow <kidus2011@gmail.com>
2022-12-12 23:38:29 -0800

    onscroll animation

--- src/components/boxOne.jsx
+++ src/components/boxOne.jsx
@@ -4,29 +4,44 @@ import { motion } from "framer-motion";
 const BoxOne = () => {
   const [isAnimating, setIsAnimating] = useState(false);
   return (
-    <div className="container mt-6 mx-auto w-full h-[400px] p-4 bg-blue-100 flex flex-col justify-start items-start">
-      <motion.div
-        className="w-[300px] h-[300px] mt-6 relative p-4 bg-indigo-300"
-        initial={{
-          opacity: 0.4,
-        }}
-        animate={{
-          x: isAnimating ? 600 : 0,
-          rotate: isAnimating ? 360 : 0,
-          opacity: 0.8,
-          backgroundColor: "green",
-        }}
-        transition={{
-          type: "spring",
-          stiffness: 20,
-          damping: 1,
+    <>
+      <div className="container mt-6 mx-auto w-full h-screen overflow-hidden p-4 bg-blue-100 flex flex-col justify-start items-start">
+        <motion.div
+          className="w-[300px] h-[300px] mt-6 relative p-4 bg-indigo-300"
+          initial={{
+            opacity: 1,
+          }}
+          animate={{
+            x: isAnimating ? 640 : 0,
+            rotate: isAnimating ? 360 : 0,
+            opacity: isAnimating ? 1 : 0.4,
+            backgroundColor: "green",
+          }}
+          transition={{
+            type: "spring",
+            stiffness: 20,
+            damping: 1,
 
-          // type: "tween",
-          // duration: 3,
+            // type: "tween",
+            // duration: 3,
+          }}
+          onClick={() => setIsAnimating(!isAnimating)}
+        >
+          <h1>Hello Test</h1>
+        </motion.div>
+      </div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{
+          duration: 1,
         }}
-        onClick={() => setIsAnimating(!isAnimating)}
-      ></motion.div>
-    </div>
+      >
+        <div className="flex justify-center mt-10 items-center rounded-full h-[200px] bg-blue-200">
+          What
+        </div>
+      </motion.div>
+    </>
   );
 };
 
