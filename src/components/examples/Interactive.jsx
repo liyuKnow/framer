6de884f43ef73348d4721d31df3a1ebb729147c0
liyuKnow <kidus2011@gmail.com>
2022-12-16 23:05:16 -0800

    all examples

--- src/components/examples/Interactive.jsx
+++ src/components/examples/Interactive.jsx
@@ -1,34 +1,79 @@
-import React, { useState } from "react";
-import { motion } from "framer-motion";
+import React from "react";
+import { motion, useAnimation } from "framer-motion";
 
 const Interactive = () => {
-  const [isAnimating, setIsAnimating] = useState(false);
+  const animControl = useAnimation();
   return (
     <>
       <div className="w-full h-screen overflow-hidden p-4 bg-blue-100 flex flex-col justify-start items-start">
-        <motion.div
-          className="w-[300px] h-[300px] mt-6 relative p-4 bg-indigo-300"
-          initial={{
-            opacity: 1,
-          }}
-          animate={{
-            x: isAnimating ? 500 : 0,
-            rotate: isAnimating ? 360 : 0,
-            opacity: isAnimating ? 1 : 0.4,
-            backgroundColor: "green",
-          }}
-          transition={{
-            type: "spring",
-            stiffness: 5,
-            damping: 1,
+        <div className="flex flex-row gap-10">
+          <button
+            onClick={() => {
+              animControl.start({
+                x: 600,
+                transition: {
+                  duration: 2,
+                },
+              });
+            }}
+            className="w-[130px] h-[50px] rounded hover:bg-green-700 bg-green-300 px-4 py-2 text-white"
+          >
+            Move Right
+          </button>
+          <button
+            onClick={() => {
+              animControl.start({
+                x: 0,
+                transition: {
+                  duration: 1,
+                },
+              });
+            }}
+            className="w-[130px] h-[50px] rounded hover:bg-green-700 bg-green-300 px-4 py-2 text-white"
+          >
+            Move Left
+          </button>
+          <button
+            onClick={() => {
+              animControl.start({
+                borderRadius: "50%",
+                transition: {
+                  duration: 1,
+                },
+              });
+            }}
+            className="w-[130px] h-[50px] rounded hover:bg-green-700 bg-green-300 px-4 py-2 text-white"
+          >
+            Circle
+          </button>
+          <button
+            onClick={() => {
+              animControl.start({
+                borderRadius: 0,
+                transition: {
+                  duration: 1,
+                },
+              });
+            }}
+            className="w-[130px] h-[50px] rounded hover:bg-green-700 bg-green-300 px-4 py-2 text-white"
+          >
+            Square
+          </button>
+          <button
+            onClick={() => {
+              animControl.stop();
+            }}
+            className="w-[130px] h-[50px] rounded hover:bg-green-700 bg-green-300 px-4 py-2 text-white"
+          >
+            Stop
+          </button>
+        </div>
 
-            // type: "tween",
-            // duration: 3,
-          }}
-          onClick={() => setIsAnimating(!isAnimating)}
-        >
-          <h1>Hello Test</h1>
-        </motion.div>
+        <motion.div
+          animate={animControl}
+          con
+          className="w-[300px] h-[300px] mt-6 relative p-4 bg-indigo-300"
+        ></motion.div>
       </div>
     </>
   );
